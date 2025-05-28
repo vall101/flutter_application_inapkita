b7aee0c51560ab77b1f2469e785c1457f3fadf92
yashaasterisk <yashaasterisk@gmail.com>
2025-05-28 11:55:35 +0700

    initupdate

---
+++ lib/Screen/tampilan_pemilik.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TampilanPemilik extends StatefulWidget {
+  const TampilanPemilik({super.key});
+
+  @override
+  State<TampilanPemilik> createState() => _TampilanPemilikState();
+}
+
+class _TampilanPemilikState extends State<TampilanPemilik> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
