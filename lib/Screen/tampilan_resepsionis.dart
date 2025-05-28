2c4425d6e362d593c6d35594e241166da4e2d6d4
AsriAsrigum54 <asriimbuhg0708@gmail.com>
2025-05-28 12:19:07 +0700

    awalan

---
+++ lib/Screen/tampilan_resepsionis.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HalamanResepsionis extends StatefulWidget {
+  const HalamanResepsionis({super.key});
+
+  @override
+  State<HalamanResepsionis> createState() => _HalamanResepsionisState();
+}
+
+class _HalamanResepsionisState extends State<HalamanResepsionis> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
