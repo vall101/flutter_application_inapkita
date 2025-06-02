1bd444de00e9d7509a5a53322fd79da9af75f141
Khinta08 <tataaalonely@gmail.com>
2025-06-02 15:24:33 +0700

    halaman_menudeskripsi

---
+++ lib/screen/pesankamar/menudeskripsi.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class menudeskripsi extends StatefulWidget {
+  const menudeskripsi({super.key});
+
+  @override
+  State<menudeskripsi> createState() => _menudeskripsiState();
+}
+
+class _menudeskripsiState extends State<menudeskripsi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
