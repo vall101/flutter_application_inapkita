066f60a18884c87f7a1550cd58e65e263544b5c1
VaniaChrisalia22 <vchrxhyn@gmail.com>
2025-06-02 15:29:48 +0700

    rating

---
+++ lib/screen/rating/rating.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Rating extends StatefulWidget {
+  const Rating({super.key});
+
+  @override
+  State<Rating> createState() => _RatingState();
+}
+
+class _RatingState extends State<Rating> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
