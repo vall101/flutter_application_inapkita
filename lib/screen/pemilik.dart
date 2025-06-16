59a79d72256858787789ba8733859bd6c139c7db
Khinta08 <tataaalonely@gmail.com>
2025-06-16 17:48:44 +0700

    pemilik

--- lib/screen/pemilik.dart
+++ lib/screen/pemilik.dart
@@ -1,15 +1,1 @@
 import 'package:flutter/material.dart';
-
-class Pemilik extends StatefulWidget {
-  const Pemilik({super.key});
-
-  @override
-  State<Pemilik> createState() => _PemilikState();
-}
-
-class _PemilikState extends State<Pemilik> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
