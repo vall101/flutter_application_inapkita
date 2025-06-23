54fe663e97884c50ca2358401f360a02f7dd220e
Asrigum54 <asriimbuhg0708@gmail.com>
2025-06-23 11:32:09 +0700

    update_ketersediankamar_part3 update dan menambahkan di menu ketersediaann kamar

--- lib/screen/resepsionis/ketersediaan_kamar.dart
+++ lib/screen/resepsionis/ketersediaan_kamar.dart
@@ -88,7 +88,7 @@ class _KetersediaanKamarScreenState extends State<KetersediaanKamarScreen> {
               onPressed: () => Navigator.pop(context),
               child: const Text('Tutup'),
             ),
-          ],
+          ],//
         ),
       );
     }
