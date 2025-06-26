ef85b9db25d2d521fd077a8d799030474025f07d
yashaasterisk <yashaasterisk@gmail.com>
2025-06-26 20:25:10 +0700

    Update initial route to '/splash' and add SplashPage route in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'screen/login/login_selection_screen.dart';
 import 'screen/rating/rating.dart';
 import 'screen/rating/input_rating.dart';
 import 'screen/diskon/menudiskon.dart';
+import 'splash_page.dart';
 
 void main() {
   runApp(const InapKitaApp());
@@ -27,7 +28,7 @@ class InapKitaApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/splash',
       routes: {
         '/': (context) => const LoginSelectionScreen(),
         '/main': (context) => const MainNavigation(),
@@ -40,6 +41,7 @@ class InapKitaApp extends StatelessWidget {
         '/diskon': (context) => const MenuDiskonPage(),
         '/pembayaran': (context) => const MenuPembayaran(totalHarga: 1850000),
         '/pembayaran2': (context) => const MenuPembayaran2(totalHarga: 1850000),
+        '/splash': (context) => const SplashPage(),
       },
       onGenerateRoute: (settings) {
         if (settings.name == '/deskripsi') {
